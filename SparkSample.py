eb65fd3a137e1148a96991ec6884fed3c1357e80
Bibin Nahas <bbnnaz@gmail.com>
2021-02-24 11:08:16 +0530

    new module to fix the read issue

--- SparkSample.py
+++ SparkSample.py
@@ -5,7 +5,7 @@ import os
 os.environ["AWS_SECRET_KEY_ID"] = "**"
 os.environ["AWS_SECRET_ACCESS_KEY"] = "**"
 os.environ[
-    'PYSPARK_SUBMIT_ARGS'] = '--packages com.amazonaws:aws-java-sdk-pom:1.10.34,org.apache.hadoop:hadoop-aws:2.7.2 ' \
+    'PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.hadoop:hadoop-aws:2.7.4 ' \
                              'pyspark-shell '
 
 spark = SparkSession.builder.appName("SimpleApp").getOrCreate()
@@ -21,14 +21,23 @@ print(df_pandas)
 # import com.amazonaws.SDKGlobalConfiguration
 # System.setProperty(SDKGlobalConfiguration.ENABLE_S3_SIGV4_SYSTEM_PROPERTY, "true")
 
+# Setup spark to use s3, and point it to the moto server.
+# hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+# hadoop_conf.set("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+# hadoop_conf.set("fs.s3a.access.key", os.environ.get('AWS_SECRET_KEY_ID'))
+# hadoop_conf.set("fs.s3a.secret.key", os.environ.get('AWS_SECRET_ACCESS_KEY'))
+# hadoop_conf.set("fs.s3a.endpoint", "s3.us-east-2.amazonaws.com")
+# hadoop_conf.set("com.amazonaws.services.s3.enableV4", "true")
+# hadoop_conf.set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.BasicAWSCredentialsProvider")
+
 spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", os.environ.get('AWS_SECRET_KEY_ID'))
 spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", os.environ.get('AWS_SECRET_ACCESS_KEY'))
 spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "false")
+spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
 spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a"
-                                                                        ".BasicAWSCredentialsProvider")
-spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.us-east-2.amazonaws.com")
-spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+                                                                        ".TemporaryAWSCredentialsProvider")
+# spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "s3.us-east-2.amazonaws.com")
+# spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
 
 df = spark.read.text('s3a://sparkdemobucketeygds/docker')
 
--- sparkdemo.py
+++ sparkdemo.py
@@ -12,14 +12,14 @@ spark = SparkSession.builder.getOrCreate()
 
 # hadoop_conf = spark.sparkContext._jsc
 
-spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", access_key_id)
-spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", secret_access_key)
-spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
-spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a"
-                                                                        ".BasicAWSCredentialsProvider")
-spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "us-east-2.amazonaws.com")
-spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
+# spark._jsc.hadoopConfiguration().set("fs.s3a.access.key", access_key_id)
+# spark._jsc.hadoopConfiguration().set("fs.s3a.secret.key", secret_access_key)
+# spark._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+# spark._jsc.hadoopConfiguration().set("com.amazonaws.services.s3.enableV4", "true")
+# spark._jsc.hadoopConfiguration().set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a"
+#                                                                         ".BasicAWSCredentialsProvider")
+# spark._jsc.hadoopConfiguration().set("fs.s3a.endpoint", "us-east-2.amazonaws.com")
+# spark._jsc.hadoopConfiguration().set("fs.s3a.path.style.access", "true")
 
 df = spark.read.text('s3a://sparkdemobucketeygds/docker').show
 

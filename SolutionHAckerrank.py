262a868df8bae4ffbf6fc5155c9c23a910e168c4
Bibin Nahas <bbnnaz@gmail.com>
2021-04-08 01:12:24 +0530

    Hackerrank problem solution for cluster performance. (Few test cases are failing)

--- SolutionHAckerrank.py
+++ SolutionHAckerrank.py
@@ -10,26 +10,32 @@ def maximumClusterQuality(speed, reliability, maxMachines, speed_count):
     reliabilityCombs = []
     minReliabilities = []
 
+    # collect speed combinations
     for i in range(speed_count):
         output.append(speed[i] * reliability[i])
 
     for i in range(2, maxMachines + 1):
         speedCombinationSums.append([sum(x) for x in itertools.permutations(speed, i)])
 
+    # form the speed's array
     for x in speedCombinationSums:
         for y in x:
             speedsSplit.append(y)
 
+    # collect reliability combinations
     for i in range(2, maxMachines + 1):
         reliabilityCombs.append([x for x in itertools.permutations(reliability, i)])
 
+    # Find minimum of each combinations
     for x in reliabilityCombs:
         for y in x:
             minReliabilities.append(min(y))
 
+    # find all the performance values and append to output array
     for i in range(len(speedsSplit)):
         output.append(speedsSplit[i] * minReliabilities[i])
 
+    # return the max value which is our solution
     return max(output)
 
 
